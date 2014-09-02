6d7f0b740cf9e5a5112367c89162134f72119079
Tristan <tldmartin@gmail.com>
2014-09-02 15:01:08 +1000

    wins correctly identified

--- src/connectfour/Board.scala
+++ src/connectfour/Board.scala
@@ -19,20 +19,29 @@ class Board(width: Int, height: Int) {
   def checkForWinAt(column: Int, row: Int): Boolean = {
     val playerTile = tileAt(column, row)
     def checkByIncrement(inc: (Int, Int)): Boolean = {
+      println("checking by in:" + inc)
+      var line = 0
       for (i <- -winningLineSize to winningLineSize) {
         val x = column + i * inc._1
         val y = row + i * inc._2
-        if (tileAt(x, y) != playerTile)
-          return false
+        println("checking [" + x + "," + y + "] in case " + tileAt(x, y) + "==" + playerTile + " line is" + line)
+        if (tileAt(x, y) == playerTile) {
+          line += 1
+          if (line == winningLineSize)
+            return true
+        } else {
+          line = 0
+        }
       }
-      true
+      println("returning false")
+      return false
     }
     val vertical = (0, 1)
     val horizontal = (1, 0)
     val diagDown = (1, 1)
     val diagUp = (1, -1)
 
-    return (checkByIncrement(vertical) | checkByIncrement(horizontal) | checkByIncrement(diagDown) | checkByIncrement(diagUp))
+    return (checkByIncrement(vertical) || checkByIncrement(horizontal) || checkByIncrement(diagDown) || checkByIncrement(diagUp))
   }
 
   def findTopEmptyRow(column: Int): Int = {
@@ -52,7 +61,8 @@ class Board(width: Int, height: Int) {
     val row = findTopEmptyRow(column)
     if (row > -1) {
       tiles(column)(row) = tile
-      val move = new Move(this.copyBoard, column, row, checkForWinAt(column, row), tile)
+      val won = checkForWinAt(column, row)
+      val move = new Move(this.copyBoard, column, row, won, tile)
       return move
     } else {
       return null
